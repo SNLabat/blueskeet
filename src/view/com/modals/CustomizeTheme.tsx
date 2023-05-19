95b85b1c842a144eed44fa1707b0ded018e6efa0
SNLabat <snlabat@gmail.com>
2023-05-19 14:13:25 -0500

    Theme Selection progress

--- src/view/com/modals/CustomizeTheme.tsx
+++ src/view/com/modals/CustomizeTheme.tsx
@@ -1,60 +1,62 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, Modal } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { observer } from 'mobx-react-lite'
 import { usePalette } from 'lib/hooks/usePalette';
+import { useStores } from 'state/index'
+import { ScrollView } from './util'
 
-const CustomizeThemeModal = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+export function CustomizeThemeModal() {
+  const store = useStores()
   const pal = usePalette('default')
+  const onPressDone = React.useCallback(() => {
+    // replace shell.closeModal() with your own modal closing function
+    store.shell.closeModal() 
+  }, [store])
 
-  const toggleModal = () => {
-    setIsModalVisible(!isModalVisible);
-  };
+  const themes = React.useMemo(() => {
+    // replace this list with your actual themes data
+    const themeList = ['Theme 1', 'Theme 2', 'Theme 3', 'Theme 4', 'Theme 5', 'Theme 6', 'Theme 7', 'Theme 8', 'Theme 9', 'Theme 10']
+    return themeList
+  }, [store])
 
   return (
-    <View testID="contentLanguagesModal" style={[pal.view, styles.container]}>
-    <Text style={[pal.text, styles.title]}>Select a Theme</Text>
-    <Text style={[pal.text, styles.description]}>
-      Customize blueskeet to your liking.
-    </Text>
-      {isModalVisible && (
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalTitle}>Customize Theme</Text>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 1 selected')}>
-            <Text style={styles.optionText}>Option 1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 2 selected')}>
-            <Text style={styles.optionText}>Option 2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 3 selected')}>
-            <Text style={styles.optionText}>Option 3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 4 selected')}>
-            <Text style={styles.optionText}>Option 4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 5 selected')}>
-            <Text style={styles.optionText}>Option 5</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 6 selected')}>
-            <Text style={styles.optionText}>Option 6</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 7 selected')}>
-            <Text style={styles.optionText}>Option 7</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 8 selected')}>
-            <Text style={styles.optionText}>Option 8</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 9 selected')}>
-            <Text style={styles.optionText}>Option 9</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.option} onPress={() => console.log('Option 10 selected')}>
-            <Text style={styles.optionText}>Option 10</Text>
-          </TouchableOpacity>
-          <Button title="Close" onPress={toggleModal} />
-        </View>
-      )}
+    <View testID="customizeThemeModal" style={[pal.view, styles.container]}>
+      <Text style={[pal.text, styles.title]}>Select a Theme</Text>
+      <Text style={[pal.text, styles.description]}>Customize blueskeet to your liking.</Text>
+      <ScrollView style={styles.scrollContainer}>
+        {themes.map((theme, index) => (
+          <ThemeToggle
+            key={index}
+            name={theme}
+          />
+        ))}
+      </ScrollView>
+      <TouchableOpacity style={styles.btn} onPress={onPressDone}>
+        <Text style={styles.btnText}>Done</Text>
+      </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
+
+const ThemeToggle = observer(
+  ({name}: {name: string}) => {
+    const store = useStores()
+    const pal = usePalette('default')
+
+    const onPress = React.useCallback(() => {
+      // replace toggleTheme with your function to change the theme
+      store.preferences.toggleTheme(name) 
+    }, [store, name])
+
+    return (
+      <TouchableOpacity
+        style={styles.option}
+        onPress={onPress}>
+        <Text style={styles.optionText}>{name}</Text>
+      </TouchableOpacity>
+    )
+  },
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -62,27 +64,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    fontSize: 20,
-    color: 'blue',
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.1)',
-  },
-  modalTitle: {
-    fontSize: 20,
+  title: {
+    fontSize: 24,
     marginBottom: 12,
+  },
+  description: {
+    marginBottom: 10,
+  },
+  scrollContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
   },
   option: {
     paddingVertical: 10,
@@ -90,6 +81,16 @@ const styles = StyleSheet.create({
   },
   optionText: {
     fontSize: 18,
+  },
+  btn: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    backgroundColor: 'blue', // replace with your actual button color
+  },
+  btnText: {
+    color: 'white', // replace with your actual button text color
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
